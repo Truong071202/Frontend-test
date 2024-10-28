35b141c2025afa33de233d2463d834a1ce4ffbef
Truong <truongdang2k2dn@gmail.com>
2024-10-28 10:16:33 +0700

    "merge"

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import HomePage from "./pages/HomePage";
 function App() {
   return (
     <Routes>
-      <Route path="/Frontend-test" element={<HomePage />} />
       <Route path="/" element={<HomePage />} />
+      <Route path="/Frontend-test" element={<HomePage />} />
+
     </Routes>
   );
 }
